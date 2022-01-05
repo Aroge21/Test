520e368a5a0d4d88fc70e416b557812ed3f1b0e4
Aroge21 <alexrogers2345@gmail.com>
2022-01-05 14:17:09 -0600

    first commet

---
+++ Test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+
+    /* Type your code here. */
+    string userInput;
+
+    cout << "Enter Sample text: " << endl;
+
+    getline(cin, userInput);
+
+    cout << "You entered: " << userInput << endl;
+    cout << endl;
+
+
+    return 0;
+}
